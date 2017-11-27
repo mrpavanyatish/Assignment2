721aa7418f7ae7c9a13e1df21b724137bf35d65d
mrpav <mrpav@10.0.0.196>
2017-11-27 17:08:48 -0600

    submit

---
+++ CalculateAge/src/calculateAge.java
@@ -0,0 +1,76 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class calculateAge {
+
+	public static void main(String[] args) throws ParseException {
+		
+		Calendar todayCalendar = Calendar.getInstance();
+		
+		SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
+		
+		Date dob = dateFormat.parse("11/28/1991");
+		
+		Calendar dobCalendar = Calendar.getInstance();
+		
+		dobCalendar.setTime(dob);
+		
+		int age = todayCalendar.get(Calendar.YEAR)-dobCalendar.get(Calendar.YEAR);
+		
+		int months = todayCalendar.get(Calendar.MONTH)-dobCalendar.get(Calendar.MONTH);
+		
+		int day = todayCalendar.get(Calendar.DAY_OF_MONTH)-dobCalendar.get(Calendar.DAY_OF_MONTH);
+		
+		if(months<0)
+		{
+			age=age-1;
+			months = todayCalendar.get(Calendar.MONTH)+1;
+			day = todayCalendar.get(Calendar.DAY_OF_MONTH);
+		}
+		else if(months==0)
+		{
+			if(day<0)
+			{
+				age=age-1;
+				months = todayCalendar.get(Calendar.MONTH) + 1;
+				day = todayCalendar.get(Calendar.DAY_OF_MONTH);
+			}
+			else if(day==0)
+			{
+				
+				System.out.println("Hurray!!! Its your BirthDay");
+				day=0;
+			}
+			else
+			{
+				months=0;
+				day=Math.abs(dobCalendar.get(Calendar.DAY_OF_MONTH)-todayCalendar.get(Calendar.DAY_OF_MONTH));
+
+				
+			}	
+		}
+		else
+		{
+			if(day<0)
+			{
+				day = Math.abs(day = todayCalendar.get(Calendar.DAY_OF_MONTH)-dobCalendar.get(Calendar.DAY_OF_MONTH));
+
+			}
+			else if(day==0)
+			{
+				
+			}
+			else
+			{
+				months--;
+				months=months+1;
+				day = todayCalendar.get(Calendar.DAY_OF_MONTH);
+			}
+		}
+		System.out.println("Person Age is " + age +" years " + months + " months "  + day +" days ");
+
+	}
+
+}
